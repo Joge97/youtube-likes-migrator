2d36becbd39d7beb9f3b4e0b1eb3f1613e242c58
Joge <joge@application-studios.com>
2018-02-20 08:04:49 +0100

    feat(YoutubeAPI): implement video rating

--- src/youtube-api.ts
+++ src/youtube-api.ts
@@ -16,10 +16,45 @@ export class YoutubeAPI {
         });
     }
 
+    public getCurrentUserChannels(): Promise<any[]> {
+        return this.apiEndpoint.channels.list({
+            part: 'contentDetail',
+            mine: true
+        });
+    }
+
     public getCurrentUserPlaylists(): Promise<any[]> {
         return this.apiEndpoint.playlists.list({
             part: 'snippet,contentDetails',
             mine: true
+        });
+    }
+
+    public getCurrentUserPlaylistItems(playlistId: string): Promise<any[]> {
+        return this.apiEndpoint.playlistItems.list({
+            part: 'snippet',
+            playlistId
+        });
+    }
+
+    public likeVideo(id: string): Promise<any> {
+        return this.apiEndpoint.videos.rate({
+            rating: 'like',
+            id
+        });
+    }
+
+    public dislikeVideo(id: string): Promise<any> {
+        return this.apiEndpoint.videos.rate({
+            rating: 'dislike',
+            id
+        });
+    }
+
+    public unrateVideo(id: string): Promise<any> {
+        return this.apiEndpoint.videos.rate({
+            rating: 'none',
+            id
         });
     }
 }
