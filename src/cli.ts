0f7f5b2e379993190d3b17593ba6cf5cb8e50762
Joge <joge@application-studios.com>
2018-02-20 08:05:03 +0100

    chore(CLI): add some comments

--- src/cli.ts
+++ src/cli.ts
@@ -18,6 +18,9 @@ commander
         try {
             await youtubeAPI.init(keys.web.client_id, keys.web.client_secret, keys.web.redirect_uris[0]);
             // TODO migrate likes
+            // 1. get channels from old account
+            // 2. get likes playlist from old account
+            // 3. like 50 (this is the limit) videos each day with the new account
             console.log(`Successfully migrated likes`);
             process.exit(0);
         } catch(error) {
